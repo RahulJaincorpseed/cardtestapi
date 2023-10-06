470f16d27a8f65ef7f7837be9271a92201aa8e7c
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-10-06 09:54:42 +0530

    add card data and also add functionality of remove card and also add some ui changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import CardComponent from './Component/CardComponent';
+import logo from "./logo.svg"
+import "./App.css"
+import CardComponent from "./Component/CardComponent"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import LoadingSpinner from "./Component/LoadingSpinner"
 
 function App() {
+  const [cardData, setCardData] = useState([])
+  const [addCard, setAddCard] = useState(3)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchCardData()
+  }, [])
+
+  const addCardFun = () => {
+    setAddCard((prev) => prev)
+  }
+
+  const createCard = async () => {
+    setAddCard((prev) => prev + 1)
+  }
+
+  const fetchCardData = async () => {
+    const apiResponse = await axios.get("https://swapi.dev/api/people")
+    console.log("response", apiResponse.data.results)
+    setCardData(apiResponse.data.results)
+    setLoading(false);
+  }
+
+  console.log("card data", cardData)
+
   return (
     <div className="main">
-      <h1 className='main-heading'>Card Data</h1>
-      <div className='card-box'>
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
+      <div className="box-heading">
+        <h1 className="heading">Card Data</h1>
+        <button className="card-button" onClick={(e) => createCard(e)}>
+          Create card
+        </button>
+      </div>
+
+      <div className="card-box">
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          cardData
+            .slice(0, addCard)
+            .map((card, index) => (
+              <CardComponent
+                addCard={addCard}
+                cardindex={index}
+                cardfilms={card.films}
+                cardname={card.name}
+                cardheight={card.height}
+                setAddCard = {setAddCard}
+                
+              />
+            ))
+        )}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Component/CardComponent.js
+++ src/Component/CardComponent.js
@@ -1,9 +1,30 @@
+import axios from "axios";
 import React from "react";
+import "./CardComponent.css";
 
-const CardComponent = () => {
+const CardComponent = ({cardname, cardheight, cardindex, cardfilms, addCard, setAddCard}) => {
+   
+const removeCard = (id) =>{
+  console.log(id);
+  setAddCard((prev)=> (prev -1))
+}
+
   return(
-    <div>
-        <h1>Card</h1>
+    <div className="single-card" index={cardindex}>
+        <h1 className="card-heading">Card</h1>
+        <p className="card-data">card Name: {cardname}</p>
+        <p className="card-data">card Height: {cardheight}</p>
+        <p className="card-data">Film List</p>
+        
+        {addCard > 3 ? <i onClick={(id)=> removeCard(cardindex)} class="fa-regular hide-card-data fa-circle-xmark"></i>: ""}
+        <div className="film-list-box">
+        {cardfilms.map((film, index)=>(
+            <div index={index}>
+                <p>{film}</p>
+            </div>
+        ))}
+        </div>
+
     </div>
   )
 };
